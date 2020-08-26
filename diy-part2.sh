9d846ed2899f66b58259d864201eb8ff600b5ecd
yasum2006 <32138091+yasum2006@users.noreply.github.com>
2020-08-26 13:57:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.6.192/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/WRT1900 V2/g' package/base-files/files/bin/config_generate
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
